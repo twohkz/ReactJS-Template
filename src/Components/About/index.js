46f7724d4330d3ff761f704e146979ab5597f6e9
twohkz <web2bhw@gmail.com>
2019-12-05 19:37:49 +0100

    Added the Info Section

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import {AboutSection, AboutInfo, AboutTitle, AboutSpan, AboutDir, AboutDesc, ABoutA } from './style.js';
 
 
 const  About = () => {
       return (
-        <div >
-          About
-        </div>
+        <AboutSection>
+            <div class="container">
+                <AboutInfo>
+                    <AboutTitle><AboutSpan>This is</AboutSpan> Me</AboutTitle>
+                    <AboutDir>Creative Director</AboutDir>
+                    <AboutDesc>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim est <a href="#">explicabo</a> maxime quibusdam quaerat nihil aliquid aperiam deleniti, repellendus dolorum. Possimus nisi cupiditate ad veritatis?
+                    </AboutDesc>
+                    <AboutDesc>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    </AboutDesc>
+                </AboutInfo>
+            </div>
+        </AboutSection>
       )
     }
   
---
+++ src/Components/About/style.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const AboutSection = styled.div`
+    height: 500px;
+    background: url('/images/creative/about-bg.jpg');
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed
+    `
+
+
+export const AboutInfo = styled.div `
+    padding-top: 100px;
+    width: 50%;
+    float: right;
+    `
+
+export const AboutTitle = styled.h2`
+    font-weight: bold;
+    font-size: 50px
+    `
+
+
+export const AboutSpan = styled.span`
+    font-weight: normal
+    `
+
+export const AboutDir = styled.h4`
+    font-size: 40px;
+    color: #eb5424;
+    margin-bottom: 20px
+    `
+
+export const AboutDesc = styled.p `
+    color: #000;
+    margin-bottom: 15px;
+    line-height: 1.8
+    `
+
+export const AboutA = styled.a`
+    text-decoration: none
+    `
