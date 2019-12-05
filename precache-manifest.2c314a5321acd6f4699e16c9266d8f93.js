0cc5fcb8a33b36936eb2f43e4ec97f930a4fa0d8
twohkz <web2bhw@gmail.com>
2019-12-05 21:32:46 +0100

    Updates

---
+++ precache-manifest.2c314a5321acd6f4699e16c9266d8f93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a3007ff9df2a1e95495192a07c5fae",
+    "url": "/twohkz/ReactJS-Template/index.html"
+  },
+  {
+    "revision": "921e8868873dc791a282",
+    "url": "/twohkz/ReactJS-Template/static/css/main.c4dedb2c.chunk.css"
+  },
+  {
+    "revision": "33568e2b5daa8bb9ba98",
+    "url": "/twohkz/ReactJS-Template/static/js/2.0e134dd0.chunk.js"
+  },
+  {
+    "revision": "921e8868873dc791a282",
+    "url": "/twohkz/ReactJS-Template/static/js/main.1c7551f7.chunk.js"
+  },
+  {
+    "revision": "d60767dfd368a0f5745b",
+    "url": "/twohkz/ReactJS-Template/static/js/runtime-main.216db005.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/twohkz/ReactJS-Template/precache-manifest.2c314a5321acd6f4699e16c9266d8f93.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/twohkz/ReactJS-Template/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
